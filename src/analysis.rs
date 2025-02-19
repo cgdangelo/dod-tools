62f012b229b3a16fbaa4a40224044814283420fc
Charles D'Angelo <c.g.dangelo@gmail.com>
2025-02-18 21:10:08 -0500

    indentation fight

--- src/analysis.rs
+++ src/analysis.rs
@@ -64,12 +64,13 @@ pub struct AnalyzerState {
 
 impl AnalyzerState {
     fn find_player_by_client_index_mut(&mut self, client_index: u8) -> Option<&mut Player> {
-        self.players
-            .iter_mut()
-            .find(|player| match player.connection_status {
-                ConnectionStatus::Connected { client_id } => client_id == client_index,
-                _ => false,
-            })
+        self.players.iter_mut().find(|player| {
+            if let ConnectionStatus::Connected { client_id } = player.connection_status {
+                client_id == client_index
+            } else {
+                false
+            }
+        })
     }
 
     fn find_player_by_global_id_mut(&mut self, global_id: &PlayerGlobalId) -> Option<&mut Player> {
@@ -126,83 +127,75 @@ pub fn use_player_updates(state: &mut AnalyzerState, event: &AnalyzerEvent) {
         let existing_player_in_slot =
             state.find_player_by_client_index_mut(svc_update_user_info.index);
 
-        match existing_player_in_slot {
-            Some(current_player) => {
-                // A new player has taken over the slot from an old player
-                if current_player.player_global_id != player_global_id {
-                    // Indicate that the old player is disconnected now
-                    current_player.connection_status = ConnectionStatus::Disconnected;
+        if let Some(current_player) = existing_player_in_slot {
+            // A new player has taken over the slot from an old player
+            if current_player.player_global_id != player_global_id {
+                // Indicate that the old player is disconnected now
+                current_player.connection_status = ConnectionStatus::Disconnected;
 
-                    // Try to find an existing record of the player
-                    if let Some(player) = state.find_player_by_global_id_mut(&player_global_id) {
-                        player.name = player_name;
-                        player.connection_status = ConnectionStatus::Connected {
-                            client_id: svc_update_user_info.index,
-                        };
-                    } else {
-                        let mut new_player = Player::new(player_global_id);
-
-                        new_player
-                            .with_connection_status(ConnectionStatus::Connected {
-                                client_id: svc_update_user_info.index,
-                            })
-                            .with_name("");
-
-                        state.players.push(new_player);
-                    }
-                } else {
-                    current_player.name = player_name;
-                }
-            }
-
-            None => {
-                let mut new_player = Player::new(player_global_id);
-
-                new_player
-                    .with_connection_status(ConnectionStatus::Connected {
+                // Try to find an existing record of the player
+                if let Some(player) = state.find_player_by_global_id_mut(&player_global_id) {
+                    player.name = player_name;
+                    player.connection_status = ConnectionStatus::Connected {
                         client_id: svc_update_user_info.index,
-                    })
-                    .with_name("");
+                    };
+                } else {
+                    let mut new_player = Player::new(player_global_id);
 
-                state.players.push(new_player);
+                    new_player
+                        .with_connection_status(ConnectionStatus::Connected {
+                            client_id: svc_update_user_info.index,
+                        })
+                        .with_name("");
+
+                    state.players.push(new_player);
+                }
+            } else {
+                current_player.name = player_name;
             }
+        } else {
+            let mut new_player = Player::new(player_global_id);
+
+            new_player
+                .with_connection_status(ConnectionStatus::Connected {
+                    client_id: svc_update_user_info.index,
+                })
+                .with_name("");
+
+            state.players.push(new_player);
         }
     }
 }
 
 pub fn use_scoreboard_updates(state: &mut AnalyzerState, event: &AnalyzerEvent) {
     match event {
-        AnalyzerEvent::UserMessage(user_msg) => match user_msg {
-            Message::PClass(p_class) => {
-                let player = state.find_player_by_client_index_mut(p_class.client_index - 1);
+        AnalyzerEvent::UserMessage(Message::PClass(p_class)) => {
+            let player = state.find_player_by_client_index_mut(p_class.client_index - 1);
 
-                if let Some(player) = player {
-                    player.class = Some(p_class.class.clone());
-                };
+            if let Some(player) = player {
+                player.class = Some(p_class.class.clone());
+            };
+        }
+
+        AnalyzerEvent::UserMessage(Message::PTeam(p_team)) => {
+            let player = state.find_player_by_client_index_mut(p_team.client_index - 1);
+
+            if let Some(player) = player {
+                player.team = Some(p_team.team.clone());
+            };
+        }
+
+        AnalyzerEvent::UserMessage(Message::ScoreShort(score_short)) => {
+            let player = state.find_player_by_client_index_mut(score_short.client_index - 1);
+
+            if let Some(player) = player {
+                player.stats = (
+                    score_short.score as i32,
+                    score_short.kills as i32,
+                    score_short.deaths as i32,
+                );
             }
-
-            Message::PTeam(p_team) => {
-                let player = state.find_player_by_client_index_mut(p_team.client_index - 1);
-
-                if let Some(player) = player {
-                    player.team = Some(p_team.team.clone());
-                };
-            }
-
-            Message::ScoreShort(score_short) => {
-                let player = state.find_player_by_client_index_mut(score_short.client_index - 1);
-
-                if let Some(player) = player {
-                    player.stats = (
-                        score_short.score as i32,
-                        score_short.kills as i32,
-                        score_short.deaths as i32,
-                    );
-                }
-            }
-
-            _ => {}
-        },
+        }
 
         _ => {}
     };

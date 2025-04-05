0ca61a33e5f2620861c204f6537d1164900fa1f4
Charles D'Angelo <c.g.dangelo@gmail.com>
2025-04-05 17:32:13 -0400

    player death should always terminate their active kill streak

--- src/analysis.rs
+++ src/analysis.rs
@@ -419,6 +419,13 @@ pub fn use_kill_streak_updates(state: &mut AnalyzerState, event: &AnalyzerEvent)
             _ => false,
         };
 
+        let victim = state.find_player_by_client_index_mut(death_msg.victim_client_index - 1);
+
+        if let Some(victim) = victim {
+            // End the victim's current streak by adding a new record
+            victim.kill_streaks.push(KillStreak::default());
+        }
+
         if is_teamkill {
             return;
         }
@@ -435,13 +442,6 @@ pub fn use_kill_streak_updates(state: &mut AnalyzerState, event: &AnalyzerEvent)
                     .kills
                     .push((current_time, death_msg.weapon.clone()));
             }
-        }
-
-        let victim = state.find_player_by_client_index_mut(death_msg.victim_client_index - 1);
-
-        if let Some(victim) = victim {
-            // End the current streak by adding a new record
-            victim.kill_streaks.push(KillStreak::default());
         }
     } else if let AnalyzerEvent::UserMessage(Message::RoundState(RoundState::Reset)) = event {
         // Active kill streaks must be terminated when round is reset (i.e., after all objectives are captured)

8c5d9ab583a63f2501f9ed904b9480012f814e63
Charles D'Angelo <c.g.dangelo@gmail.com>
2025-08-16 23:20:48 -0400

    introduce separate timing modes

--- analysis/src/time.rs
+++ analysis/src/time.rs
@@ -1,31 +1,25 @@
 use crate::{AnalyzerEvent, AnalyzerState};
 use dem::types::EngineMessage;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct GameTime {
-    #[allow(dead_code)]
-    pub origin: Instant,
-    pub offset: Duration,
-}
+    pub real_offset: Duration,
+    pub viewdemo_offset: Duration,
 
-impl Default for GameTime {
-    fn default() -> Self {
-        Self {
-            origin: Instant::now(),
-            offset: Duration::from_secs(0),
-        }
-    }
+    #[deprecated(note = "Choose an offset based on desired timing model")]
+    pub offset: Duration,
 }
 
 pub fn use_timing_updates(state: &mut AnalyzerState, event: &AnalyzerEvent) {
     if let AnalyzerEvent::EngineMessage(EngineMessage::SvcTime(svc_time)) = event
         && svc_time.time > 0.
     {
-        let mut next_time = state.current_time.clone();
-
-        next_time.offset = Duration::from_secs_f32(svc_time.time);
-
-        state.current_time = next_time;
+        state.current_time.viewdemo_offset = Duration::from_secs_f32(svc_time.time);
+        state.current_time.offset = state.current_time.viewdemo_offset;
+    } else if let AnalyzerEvent::RealTimeChange(real_offset) = event
+        && *real_offset > 0.
+    {
+        state.current_time.real_offset = Duration::from_secs_f32(*real_offset);
     }
 }

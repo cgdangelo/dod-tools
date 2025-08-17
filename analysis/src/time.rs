b7f6afe5f1ef81b8a4d8037ffcde28e3b92eaab4
Charles D'Angelo <c.g.dangelo@gmail.com>
2025-08-17 15:22:24 -0400

    remove deprecated offset

--- analysis/src/time.rs
+++ analysis/src/time.rs
@@ -10,9 +10,6 @@ pub struct GameTime {
 
     /// Timestamp that represents the value shown in the `viewdemo` window.
     pub viewdemo_offset: Duration,
-
-    #[deprecated(note = "Choose an offset based on desired timing model")]
-    pub offset: Duration,
 }
 
 impl Sub<GameTime> for GameTime {
@@ -23,22 +20,30 @@ impl Sub<GameTime> for GameTime {
     }
 }
 
-impl<'a, 'b> Sub<&'a GameTime> for &'b GameTime {
+impl<'a> Sub<&'a GameTime> for &GameTime {
     type Output = Duration;
+
     fn sub(self, rhs: &'a GameTime) -> Self::Output {
         self.viewdemo_offset - rhs.viewdemo_offset
     }
 }
 
 pub fn use_timing_updates(state: &mut AnalyzerState, event: &AnalyzerEvent) {
-    if let AnalyzerEvent::EngineMessage(EngineMessage::SvcTime(svc_time)) = event
-        && svc_time.time > 0.
-    {
-        state.current_time.viewdemo_offset = Duration::from_secs_f32(svc_time.time);
-        state.current_time.offset = state.current_time.viewdemo_offset;
-    } else if let AnalyzerEvent::RealTimeChange(real_offset) = event
-        && *real_offset > 0.
-    {
-        state.current_time.real_offset = Duration::from_secs_f32(*real_offset);
+    if let AnalyzerEvent::EngineMessage(EngineMessage::SvcTime(svc_time)) = event {
+        let offset = Duration::from_secs_f32(svc_time.time);
+
+        if offset > state.current_time.real_offset
+            || state.current_time.real_offset == Duration::ZERO
+        {
+            state.current_time.viewdemo_offset = offset;
+        }
+    } else if let AnalyzerEvent::RealTimeChange(value) = event {
+        let offset = Duration::from_secs_f32(*value);
+
+        if offset > state.current_time.real_offset
+            || state.current_time.real_offset == Duration::ZERO
+        {
+            state.current_time.real_offset = offset;
+        }
     }
 }
